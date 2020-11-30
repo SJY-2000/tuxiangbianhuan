ce365139c784e97205647fd92a3cb989551c35b1
SJY-2000 <2254789143@qq.com>
2020-11-30 14:21:49 +0800

    改

--- 几何变换/touyingbianhuan.cpp
+++ 几何变换/touyingbianhuan.cpp
@@ -1,24 +1,24 @@
-//#include<opencv2/opencv.hpp>
-//#include<iostream>
-//using namespace cv;
-//int main()
-//{
-//	Mat srcMat = imread("G:\\picture\\22.jpg", 1);
-//	Mat dstMat;
-//	if (srcMat.empty())return -1;
-//	Point2f pts1[] = {
-//		Point2f(0,0),
-//		Point2f(100,0),
-//		Point2f(0,100),
-//		Point2f(100,100)};
-//	Point2f pts2[] = {
-//		Point2f(0,0),
-//		Point2f(100,20),
-//		Point2f(0,100),
-//		Point2f(100,120)};
-//	Mat perspective_matrix = getPerspectiveTransform(pts1, pts2);
-//	warpPerspective(srcMat, dstMat, perspective_matrix, srcMat.size());
-//	imshow("src", srcMat);
-//	imshow("dst", dstMat);
-//	waitKey(0);
-//}
+#include<opencv2/opencv.hpp>
+#include<iostream>
+using namespace cv;
+int main()
+{
+	Mat srcMat = imread("G:\\picture\\22.jpg", 1);
+	Mat dstMat;
+	if (srcMat.empty())return -1;
+	Point2f pts1[] = {
+		Point2f(0,0),
+		Point2f(100,0),
+		Point2f(0,100),
+		Point2f(100,100)};
+	Point2f pts2[] = {
+		Point2f(0,0),
+		Point2f(100,20),
+		Point2f(0,100),
+		Point2f(100,120)};
+	Mat perspective_matrix = getPerspectiveTransform(pts1, pts2);
+	warpPerspective(srcMat, dstMat, perspective_matrix, srcMat.size());
+	imshow("src", srcMat);
+	imshow("dst", dstMat);
+	waitKey(0);
+}
